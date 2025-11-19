b14b8cd71ddc7261c315ce9fc49fd0ddcc9127b3
young-Vladimir1 <norkin123i@gmail.com>
2025-11-19 16:37:49 +0500

    initial-setup0.0.4.sh

--- initial-setup2.sh
+++ initial-setup2.sh
@@ -2,36 +2,41 @@
 echo "Начальная настройка Ubuntu Server"
 
 # Обновление системы
-sudo apt-get update && apt-get upgrade -y
-apt-get install -y curl wget git htop nano vim ufw fail2ban
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y curl wget git htop nano vim ufw fail2ban
 
 # Создание пользователя myuser
-adduser myuser
-usermod -aG sudo myuser
+sudo adduser --gecos "" --disabled-password myuser
+sudo usermod -aG sudo myuser
+sudo passwd
+echo "1234"
 
 # Настройка SSH
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
 
-sudo echo "Port 2222
+sudo tee -a /etc/ssh/sshd_config > /dev/null << EOF
+Port 2222
 PermitRootLogin no
 PasswordAuthentication no
 PubkeyAuthentication yes
 MaxAuthTries 3
 ClientAliveInterval 300
-ClientAliveCountMax 2" >> /etc/ssh/sshd_config
+ClientAliveCountMax 2
+EOF
 
 sudo systemctl restart ssh
 
 # Настройка фаервола
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-sudo ufw allow 2222/tcp # SSH на новом порту
-sudo ufw allow 80/tcp # HTTP
-sudo ufw allow 443/tcp # HTTPS
-sudo ufw enable
+sudo ufw allow 2222/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+echo "y" | sudo ufw enable
 
 # Настройка fail2ban
-sudo echo "[DEFAULT]
+sudo tee /etc/fail2ban/jail.local > /dev/null << EOF
+[DEFAULT]
 bantime = 3600
 findtime = 600
 maxretry = 5
@@ -43,21 +48,19 @@ port = 2222
 filter = sshd
 logpath = /var/log/auth.log
 maxretry = 3
-bantime = 3600" >> /etc/fail2ban/jail.local
-
+bantime = 3600
+EOF
 
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 
 # Настройка автоматических обновлений
 sudo apt install -y unattended-upgrades
-sudo dpkg-reconfigure unattended-upgrades
+sudo dpkg-reconfigure -plow unattended-upgrades -f noninteractive
 
-# Настройка автоматических обновлений
-
-# Редактируем конфигурацию:
-sudo echo "APT::Periodic::Update-Package-Lists "1";
+# Настройка конфигурации обновлений
+sudo tee /etc/apt/apt.conf.d/20auto-upgrades > /dev/null << EOF
+APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";
-APT::Periodic::AutocleanInterval "7";" >> /etc/apt/apt.conf.d/20auto-upgrades
-
-
+APT::Periodic::AutocleanInterval "7";
+EOF
